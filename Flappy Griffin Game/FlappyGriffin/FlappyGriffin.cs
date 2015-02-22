925323b8dbd3653953d6c64bf2a4eb95ba07e290
fr0wsTyl <frowstyl@gmail.com>
2015-02-23 01:35:48 +0200

    Added main project files

---
+++ Flappy Griffin Game/FlappyGriffin/FlappyGriffin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlappyGriffin
+{
+    class FlappyGriffin
+    {
+        static void Main()
+        {
+            Console.WriteLine("GAME TEST");
+        }
+    }
+}
