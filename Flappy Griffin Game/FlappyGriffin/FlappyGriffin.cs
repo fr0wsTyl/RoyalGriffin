37b48219138adb4362434e3b96bccf3f6c290e4c
Kristiyan Terziev <kristerziev89@gmail.com>
2015-02-28 20:21:49 +0200

    Fixed the obstacles a little bit. Still need some work. Changed the way we gain score. Now we get score if we pass successfully through an obstacle.

--- Flappy Griffin Game/FlappyGriffin/FlappyGriffin.cs
+++ Flappy Griffin Game/FlappyGriffin/FlappyGriffin.cs
@@ -9,108 +9,137 @@ using System.IO;
 
 class testGriffin
 {
+
     struct Object // Creating object with paramaters for coordinates, symbol and color.
     {
         public int x;
         public int y;
         public ConsoleColor color;
-        public char c;
+        public string c;
     }
-    // Method for printing a symbol on the console.
-    static void PrintOnPosition(int x, int y, string c, ConsoleColor color = ConsoleColor.Gray)
+    struct Obstacle
     {
+        public int x;
+        public int y1;
+        public int y2;
+        public int y3;
+        public int y4;
+        public int y5;
+        public int y6;
+        public string c;
+
+    }
+
+    // Method for printing a string on the console.
+    static void PrintStringOnPosition(int x, int y, string c, ConsoleColor color = ConsoleColor.Gray)
+    {
+
         Console.SetCursorPosition(x, y); // Coordinates of cursor position.
         Console.ForegroundColor = color;
         Console.Write(c);
+
     }
-    // Another method for printing strings on given position.
-    static void PrintStringOnPosition(int x, int y, string str, ConsoleColor color = ConsoleColor.White)
+    static void PrintObstacle(int x, int y1, int y2, int y3, int y4, int y5, int y6, string c,
+        ConsoleColor color = ConsoleColor.Green)
+    // Creating method  for pronting the obsticles.
     {
-        Console.SetCursorPosition(x, y);
         Console.ForegroundColor = color;
-        Console.Write(str);
+        Console.SetCursorPosition(x, y1);
+        Console.WriteLine(c);
+        Console.SetCursorPosition(x, y2);
+        Console.WriteLine(c);
+        Console.SetCursorPosition(x, y3);
+        Console.WriteLine(c);
+        Console.SetCursorPosition(x, y4);
+        Console.WriteLine(c);
+        Console.SetCursorPosition(x, y5);
+        Console.WriteLine(c);
+        Console.SetCursorPosition(x, y6);
+        Console.WriteLine(c);
     }
+
 
 
     static void Main()
     {
         // Setting window size and removing scrollbars.
-        int playFieldWidth = 10;
+        int playFieldWidth = 11;
         Console.BufferHeight = Console.WindowHeight = 18;
-        Console.BufferWidth = Console.WindowWidth = 70;
+        Console.BufferWidth = Console.WindowWidth = 25;
 
         // Creating the Griffin.
         Object griffin = new Object();
-        griffin.c = 'G';                  // Griffin symbol.
+        griffin.c = "G";                  // Griffin symbol.
         griffin.color = ConsoleColor.White;  // color.
         griffin.x = 10;                      // Starting coordinates of the griffin.   
         griffin.y = playFieldWidth / 2;
 
         Random randomGenerator = new Random();   // Gives a random number in given range
-        List<Object> rocks = new List<Object>(); // Creating a list of moving objects on the field.
+        List<Obstacle> Obstacles = new List<Obstacle>();
         int lives = 5;
         long score = 0;
 
-
         while (true)
         {
-            bool rockHitted = false;
-            bool lifeHitted = false;
-            bool moneyHitted = false;
+            bool obstacleHitted = false;
+            int chance = randomGenerator.Next(0, 3);
+            // Random number generates different obstacle.
+            if (chance == 0)
             {
-                int chance = randomGenerator.Next(0, 100);
-                if (chance < 1)                            // In about 1% of the time we create object "life".
-                {
-                    Object nextrock = new Object();
-                    nextrock.color = ConsoleColor.Yellow;
-                    nextrock.c = 'L';                      // Symbol.
-                    nextrock.x = Console.WindowWidth;      // Coordinates: Always appears in right end
-                    nextrock.y = randomGenerator.Next(1, playFieldWidth); // and random height 
-                    rocks.Add(nextrock);                   // Adding the object to the list of miving objects.
-
-                }
-
-                else if (chance < 10)                      // In 10% of time we create "money" object.
-                {
-                    Object nextrock = new Object();
-                    nextrock.color = ConsoleColor.Green;
-                    nextrock.c = '$';                      // Symbol.
-                    nextrock.x = Console.WindowWidth;      // Coordinates.
-                    nextrock.y = randomGenerator.Next(1, playFieldWidth);
-                    rocks.Add(nextrock);                   // Adding "money" to the list of moving objects.
-                }
-                else
-                {                                  // In the rest of the time there are only rocks moving towards the griffin
-                    Object nextrock = new Object();
-                    nextrock.color = ConsoleColor.DarkGray;
-                    nextrock.c = '^';
-                    nextrock.x = Console.WindowWidth;
-                    nextrock.y = randomGenerator.Next(1, playFieldWidth);
-                    rocks.Add(nextrock);
-                }
+                Obstacle nextObstacle = new Obstacle();
+                nextObstacle.x = Console.WindowWidth;
+                nextObstacle.y1 = 1;
+                nextObstacle.y2 = 2;
+                nextObstacle.y3 = 3;
+                nextObstacle.y4 = 4;
+                nextObstacle.y5 = playFieldWidth - 2;
+                nextObstacle.y6 = playFieldWidth -1;
+                nextObstacle.c = "#";
+                Obstacles.Add(nextObstacle);
             }
-
-            if (Console.KeyAvailable) // Checking if there is any key pressed.
+            else if (chance == 1)
+            {
+                Obstacle nextObstacle = new Obstacle();
+                nextObstacle.x = Console.WindowWidth;
+                nextObstacle.y1 = 1;
+                nextObstacle.y2 = 2;
+                nextObstacle.y3 = 3;
+                nextObstacle.y4 = playFieldWidth - 3;
+                nextObstacle.y5 = playFieldWidth - 2;
+                nextObstacle.y6 = playFieldWidth - 1;
+                nextObstacle.c = "#";
+                Obstacles.Add(nextObstacle);
+            }
+            else if (chance == 2)
+            {
+                Obstacle nextObstacle = new Obstacle();
+                nextObstacle.x = Console.WindowWidth;
+                nextObstacle.y1 = 1;
+                nextObstacle.y2 = 2;
+                nextObstacle.y3 = 3;
+                nextObstacle.y4 = 4;
+                nextObstacle.y5 = 5;
+                nextObstacle.y6 = playFieldWidth - 1;
+                nextObstacle.c = "#";
+                Obstacles.Add(nextObstacle);
+            }
+            // Checking if there is any key pressed.
+            if (Console.KeyAvailable)
             {
                 ConsoleKeyInfo keyPressed = Console.ReadKey(true);
                 while (Console.KeyAvailable)
                 {
                     Console.ReadKey(true);
                 }
-                if (keyPressed.Key == ConsoleKey.Spacebar) // Moving the griffin up. //@Updated (when player push enter button, the bird will move up)
+                // Moving the griffin up by pressing space.
+                if (keyPressed.Key == ConsoleKey.Spacebar)
                 {
                     if (griffin.y - 2 >= 1)
                     {
                         griffin.y -= 2;
                     }
                 }
-                //else if (keyPressed.Key == ConsoleKey.DownArrow)   // Moving the griffin down. //@Updated (Currently this is not needable to the game because the bird has to fall down repeatly)
-                //{
-                //    if (griffin.y + 1 < playFieldWidth)
-                //    {
-                //        griffin.y += 1;
-                //    }
-                //}
+
             }
             else //Added case in which is not available pressed key by player and the bird falls down by itself
             {
@@ -118,37 +147,54 @@ class testGriffin
                 {
                     if (griffin.y + 1 == playFieldWidth)
                     {
-                        rocks.Clear();         // If we hit a rock we start again from starting position.
-                        PrintOnPosition(griffin.x, griffin.y, "GAME OVER", ConsoleColor.Red); // switching symbol when rock is hitted.
+
+                        Obstacles.Clear();
+                        // If we hit an obstacle we start again from starting position.
+                        // switching symbol when rock is hitted.
+                        PrintStringOnPosition(griffin.x, griffin.y, "GAME OVER", ConsoleColor.Red);
                         Console.Beep(500, 1000);                                      // sound when rock is hitted.
-                        PrintOnPosition(griffin.x-5, griffin.y -5, "Press any key to restart the game", ConsoleColor.White);
+                        PrintStringOnPosition(griffin.x - 5, griffin.y - 5, "Press any key to restart the game",
+                            ConsoleColor.White);
                         Console.ReadKey();
-                        griffin.x = 10;   
+                        griffin.x = 10;
                         griffin.y = playFieldWidth / 2;
                         lives--;
 
                     }
                     else
                     {
-                        griffin.y += 1;   
+                        griffin.y += 1;
                     }
-                    
+
                 }
             }
-            List<Object> nextrocks = new List<Object>();     // Creating new list of moving objects and filling it with
-            for (int i = 0; i < rocks.Count; i++)            // the same objects but on their new positions.   
+            // Creating new list of moving objects and filling it.
+            List<Obstacle> newObstacles = new List<Obstacle>();
+            for (int i = 0; i < Obstacles.Count; i++)
             {
-                Object oldrock = rocks[i];
-                Object nextrock = new Object();
-                nextrock.x = oldrock.x - 1;
-                nextrock.y = oldrock.y;
-                nextrock.c = oldrock.c;
-                nextrock.color = oldrock.color;
+                // the same objects but on their new positions.
 
+                Obstacle oldObstacle = Obstacles[i];
+                Obstacle newObstacle = new Obstacle();
+                newObstacle.x = oldObstacle.x - 1;
+                newObstacle.y1 = oldObstacle.y1;
+                newObstacle.y2 = oldObstacle.y2;
+                newObstacle.y3 = oldObstacle.y3;
+                newObstacle.y4 = oldObstacle.y4;
+                newObstacle.y5 = oldObstacle.y5;
+                newObstacle.y6 = oldObstacle.y6;
+                newObstacle.c = oldObstacle.c;
                 // Checking if the griffin hits any moving objects.
-                if (nextrock.c == '^' && nextrock.x == griffin.x && nextrock.y == griffin.y)
+
+                if (newObstacle.x == griffin.x &&
+                   (newObstacle.y1 == griffin.y ||
+                    newObstacle.y2 == griffin.y ||
+                    newObstacle.y3 == griffin.y ||
+                    newObstacle.y4 == griffin.y ||
+                    newObstacle.y5 == griffin.y ||
+                    newObstacle.y6 == griffin.y))
                 {
-                    rockHitted = true; // If we hit a rock we lose a life.
+                    obstacleHitted = true; // If we hit an obstacle we lose a life.
                     if (lives < 1)
                     {
                         return;        // If we lose our last life the game is over.
@@ -159,59 +205,61 @@ class testGriffin
                         score = 0;
                     }
                 }
-                if (nextrock.c == '$' && nextrock.x == griffin.x && nextrock.y == griffin.y)
+                // Checking if we have succesfully passed an obstacle.
+                else if (newObstacle.x == griffin.x &&
+                    newObstacle.y1 != griffin.y &&
+                    newObstacle.y2 != griffin.y &&
+                    newObstacle.y3 != griffin.y &&
+                    newObstacle.y4 != griffin.y &&
+                    newObstacle.y5 != griffin.y &&
+                    newObstacle.y6 != griffin.y)
                 {
-                    moneyHitted = true;   // If we hit money the score increases.
-                    score += 10;
+                    score++;
                 }
 
-                if (nextrock.c == 'L' && nextrock.x == griffin.x && nextrock.y == griffin.y)
+                if (newObstacle.x > 0)
                 {
-                    lifeHitted = true;     // If we hit life we get one life bonus.
-                    lives++;
-                }
-                if (nextrock.x > 0)
-                {
-                    nextrocks.Add(nextrock);// When an object disappears from the screen a new one appears in starting position
+                    // When an object disappears from the screen a new one appears in starting position
+                    newObstacles.Add(newObstacle);
                 }
             }
-            rocks = nextrocks;         // Current list of objects saves the new objects and their new positions.
-            Console.Clear();           // Clearing the console from the old objects.
-            if (rockHitted)
-            {
-                rocks.Clear();         // If we hit a rock we start again from starting position.
-                PrintOnPosition(griffin.x, griffin.y, "X", ConsoleColor.Red); // switching symbol when rock is hitted.
-                Console.Beep(500, 1000);                                      // sound when rock is hitted.
-            }
-            else if (moneyHitted)
-            {
-                PrintOnPosition(griffin.x, griffin.y, "G", ConsoleColor.Green);
+            // Current list of objects saves the new objects and their new positions.
+            Obstacles = newObstacles;
 
-            }
-            else if (lifeHitted)
+            // Clearing the console from the old objects.
+            Console.Clear();
+            if (obstacleHitted)
             {
-                PrintOnPosition(griffin.x, griffin.y, "G", ConsoleColor.Yellow);
-                Console.Beep(1200, 200);
+                // If we hit an obstacle we start again from starting position.
+                Obstacles.Clear();
+
+                // switching symbol when rock is hitted
+                PrintStringOnPosition(griffin.x, griffin.y, "X", ConsoleColor.Red);
+                // sound when rock is hitted.
+                Console.Beep(500, 1000);
             }
-            else
+
+            else   // Printing the griffin.
             {
-                PrintOnPosition(griffin.x, griffin.y, griffin.c.ToString(), griffin.color);
+                PrintStringOnPosition(griffin.x, griffin.y, griffin.c.ToString(), griffin.color);
             }
-            foreach (Object rock in rocks) // Printing the objects from the list of moving objects.
+            foreach (Obstacle obstacle in Obstacles) // Printing the obstacles.
             {
-                PrintOnPosition(rock.x, rock.y, rock.c.ToString(), rock.color);
+                PrintObstacle(obstacle.x, obstacle.y1, obstacle.y2, obstacle.y3, obstacle.y4, obstacle.y5, obstacle.y6,
+                    obstacle.c);
             }
+
             for (int i = 0; i < Console.WindowWidth; i++) // Drowing the play field borders.
             {
-                PrintOnPosition(i, playFieldWidth, "-", ConsoleColor.Green);
-                PrintOnPosition(i, 0, "-", ConsoleColor.Green);
+                PrintStringOnPosition(i, playFieldWidth, "-", ConsoleColor.Blue);
+                PrintStringOnPosition(i, 0, "-", ConsoleColor.Blue);
             }
             // Printing info.
             PrintStringOnPosition(2, 12, "Score: " + score);
             PrintStringOnPosition(2, 13, "Lives " + lives);
-            PrintStringOnPosition(15, 12, "FLAPPY GRIFFIN", ConsoleColor.Magenta);
+            PrintStringOnPosition(5, 16, "FLAPPY GRIFFIN", ConsoleColor.Magenta);
 
-            Thread.Sleep(350); // Slows down the program so we can see what happens on the screen. We can change the speed. 
+            Thread.Sleep(300); // Slows down the program so we can see what happens on the screen. We can change the speed. 
         }
     }
 }
